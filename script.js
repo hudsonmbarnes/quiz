4534c7639a279c5fa3d50f62718c9f1f834eaca8
hudsonmbarnes <hudsonmbarnes@gmail.com>
2020-12-22 21:49:13 -0800

    finished questions

---
+++ script.js
@@ -0,0 +1,73 @@
+// variables
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
+
+
+// declaring functions and event listener for button
+function buildQuiz(){
+
+}
+
+function showResults(){
+
+}
+
+buildQuiz
+
+submitButton.addEventListener("click", showResults)
+
+// function for questions
+var myQuestions = [
+{
+    question: "What is the most sold video game of all time?",
+    answers: {
+        a: "Minecraft",
+        b: "Wii Sports",
+        c: "GTA 5"
+    },
+    correctAnswer: "a"
+},
+{    
+    question: "What was the first video game?",
+    answers: {
+        a: "Sonic",
+        b: "Space Invaders",
+        c: "Pong"
+    },
+    correctAnswer: "c"
+},
+{
+    question: "Where was Nintendo founded?",
+    answers: {
+        a: "Japan",
+        b: "USA",
+        c: "China"
+    },
+    correctAnswer: "a"
+},
+{
+    question: "Who was the founder of Steam?",
+    answers: {
+        a: "Bill Gates",
+        b: "Gabe Newell",
+        c: "Fusajiro Yamauchi"
+    },
+    correctAnswer: "b"
+},
+{
+    question: "What game is yoshi from?",
+    answers: {
+        a: "Minecraft",
+        b: "Super Mario World",
+        c: "Street fighter"
+    },
+    correctAnswer: "b"
+},
+];
+
+// function to display on page
+function buildQuiz(){
+    var output = 
+}
+
